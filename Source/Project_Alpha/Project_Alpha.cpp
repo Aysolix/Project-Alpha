24c7f9bdd221bf7e93818a1ad00de67ff705609d
Aysolix <aysolix@gmail.com>
2026-04-16 17:50:19 +0100

    Added C++ Class

---
+++ Source/Project_Alpha/Project_Alpha.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project_Alpha.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Alpha, "Project_Alpha" );
